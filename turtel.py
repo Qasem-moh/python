5407972281947a78f25d8e95a18aec19c58d8555
qasem-moh <2171246@s.aou.edu.jo>
2021-09-28 19:11:05 +0300

    py

---
+++ turtel.py
@@ -0,0 +1,14 @@
+import tkinter
+from turtle import*
+import turtle
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+color('cyan')
+begin_fill()
+
+while(True):
+    forward(200)
+    left(170)
+    if abs (pos())<1:
+        break
