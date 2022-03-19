6dd674de07135333ff0c48d55762e3085f3ab7d0
qasem-moh <2171246jo@s.aou.edu.jo>
2022-03-19 04:46:35 +0300

    Python

--- testLooping.py
+++ testLooping.py
@@ -1,5 +1,13 @@
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
-charUser = input("Enter a character: ")
-newList = [ch for ch in fruits if ch[0] == charUser]
-print(newList)
+
+# charUser = input("Enter a character: ")
+# newList = [ch for ch in fruits if ch[0] == charUser]
+# print(newList)
+# *****************************************************************************
+class MyClass:
+    x = 5
+
+
+print(MyClass)
+print(MyClass.x)
