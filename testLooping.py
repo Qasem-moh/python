c4ded262ea273948ccf0077ab27dbb1150eda39c
qasem-moh <2171246jo@s.aou.edu.jo>
2022-03-22 20:41:31 +0300

    Python

--- testLooping.py
+++ testLooping.py
@@ -34,3 +34,14 @@ class Memebers:
 
 
 Memebers()
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("John", 36)
+p2 = Person("Sara", 25)
+print(p1.name, p1.age)
