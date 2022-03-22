4b2832aa2954f067b48349147dc114e75e441dd3
qasem-moh <2171246jo@s.aou.edu.jo>
2022-03-22 21:05:28 +0300

    Python

--- testLooping.py
+++ testLooping.py
@@ -37,19 +37,29 @@ Memebers()
 
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, gender):
         self.name = name
         self.age = age
+        self.gender = gender
 
     def myfunc(self):
         return "Hello my name is " + self.name
 
     def ReturnName(self):
-        return f"Hello Mr {self.name}"
+        if self.gender == "Male":
+            return f"Hello Mr {self.name}"
+        elif self.gender == "Female":
+            return f"Helo Miss {self.name}"
+        else:
+            return f"Hello {self.name}"
 
 
-p1 = Person("John", 36)
-p2 = Person("Sara", 25)
+p1 = Person("John", 36, "Male")
+p2 = Person("Sara", 25, "Female")
+p3 = Person("Qasem", 29, "")
 print(p1.name, p1.age)
 print(p1.myfunc())
 print(p1.ReturnName())
+print(p1.ReturnName())
+print(p2.ReturnName())
+print(p3.ReturnName())
